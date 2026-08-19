87226c8ee104ff7cb5d44fb532bace40bb6c401b
dharineeshv <dharineeshv18@gmail.com>
2026-08-19 11:12:53 +0530

    fix: re-enable google identity provider in terraform

--- terraform-clone/cognito.tf
+++ terraform-clone/cognito.tf
@@ -12,11 +12,11 @@ resource "aws_cognito_user_pool_client" "client" {
     "ALLOW_USER_SRP_AUTH",
     "ALLOW_REFRESH_TOKEN_AUTH"
   ]
-  supported_identity_providers = ["COGNITO"]
+  supported_identity_providers = ["COGNITO", "Google"]
   allowed_oauth_flows          = ["implicit", "code"]
   allowed_oauth_scopes         = ["phone", "email", "openid", "profile"]
   allowed_oauth_flows_user_pool_client = true
-  callback_urls                = ["https://dl21dk0zl04jl.cloudfront.net", "http://localhost:5500", "http://127.0.0.1:5500"]
+  callback_urls                = ["https://dl21dk0zl04jl.cloudfront.net", "https://dl21dk0zl04jl.cloudfront.net/", "https://dl21dk0zl04jl.cloudfront.net/login.html", "https://dl21dk0zl04jl.cloudfront.net/index.html", "https://dl21dk0zl04jl.cloudfront.net/register.html", "http://localhost:5500", "http://127.0.0.1:5500"]
   logout_urls                  = ["https://dl21dk0zl04jl.cloudfront.net"]
 }
 
